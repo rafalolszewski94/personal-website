05c46654fbeb6a5e8a2d230f29b5de182db74b81
Rafał Olszewski <rafal.olszewski@pomagam.pl>
2019-03-13 00:28:55 +0100

    Changed jekyll to nuxt

---
+++ pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        Rafał Olszewski
+      </h1>
+      <h2 class="subtitle">
+        orafal.dev
+      </h2>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+</style>

0ad93319364c0a3a3634850365eb95517def77c5
Rafał Olszewski <rafal.olszewski@pomagam.pl>
2019-03-13 02:24:06 +0100

    Updates

---
+++ precache-manifest.bdc859bc7d8781a1ad27de55cfd04d35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e5c2ee007439224fb0e2",
+    "url": "/js/chunk-vendors.dc4b5798.js"
+  },
+  {
+    "revision": "d462cf45be7fc7b79f2d",
+    "url": "/js/app.51e58ae7.js"
+  },
+  {
+    "revision": "6b8fdd414228fe010fbb",
+    "url": "/js/about.494770a4.js"
+  },
+  {
+    "revision": "958df85456d7b02f5a5faac4d073f5ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d462cf45be7fc7b79f2d",
+    "url": "/css/app.ed4a70a1.css"
+  }
+];
